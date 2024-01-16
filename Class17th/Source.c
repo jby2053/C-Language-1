0f99eee542820f4724fa08aa2a90f10f81528628
Jby2053 <jby2051@naver.com>
2024-01-16 21:47:04 +0900

    (2024.1.16)2차원 배열

---
+++ Class17th/Source.c
@@ -0,0 +1,69 @@
+#define _CRT_SECUER_NO_WARNINGS
+#include <stdio.h>
+#include <conio.h>   // 좌표이동을 할 수 있도록 도와주는 헤더파일
+#include <windows.h> // Coordinate
+
+
+#define up 72
+#define LEFT 75
+#define RIGHT 77
+#define DOWN 80
+
+void GotoXY(int x, int y)
+{
+	// x, y 좌표 설정
+	COORD position = { x,y };
+
+	// 커서 이동 함수
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+
+}
+
+
+int main()
+{
+	char key = 0;
+	int x = 2;
+	int y = 5;
+
+	GotoXY(x, y);
+	printf("☆");
+
+	while (1)
+	{
+		system("cls");
+		GotoXY(x, y);
+		printf("☆");
+
+		if (_kbhit()) // 키보드 입력 확인 (true/false)
+		{
+			key = _getch(); // key 입력을 받아주는 함수
+
+			if (key == -32)
+			{
+				key = _getch();
+			}
+
+			switch (key)
+			{
+			case up: if (y > 0) { y--; }
+				   break;
+			case LEFT:
+				if (x > 0)
+				{
+					x -= 2;
+				}
+				break;
+			case RIGHT: x += 2;
+				break;
+			case DOWN: y++;
+				break;
+				//default: printf("key 변수의 값 : %d\n", key);
+				//break;
+			}
+
+		}
+	}
+
+	return 0;
+}
--- Program/Solution.c
+++ Program/Solution.c
@@ -1,69 +1,39 @@
 #define _CRT_SECUER_NO_WARNINGS
 #include <stdio.h>
-#include <conio.h>   // 좌표이동을 할 수 있도록 도와주는 헤더파일
-#include <windows.h> // Coordinate
+#include <string.h>
 
+#define WIDTH 11
+#define HEIGHT 11
 
-#define up 72
-#define LEFT 75
-#define RIGHT 77
-#define DOWN 80
+char maze[WIDTH][HEIGHT];
 
-void GotoXY(int x, int y)
+void CreatMaze()
 {
-	// x, y 좌표 설정
-	COORD position = { x,y };
-	
-	// 커서 이동 함수
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+	// 0 : 빈 공간 (" ")
+	// 1 : 벽 (▒)
+	// 2 : 탈출구 (♥)
+	strcpy(maze[0],  "1111100011");
+	strcpy(maze[1],  "1111000111");
+	strcpy(maze[2],  "1100010011");
+	strcpy(maze[3],  "1001111001");
+	strcpy(maze[4],  "0011111100");
+	strcpy(maze[5],  "1100110011");
+	strcpy(maze[6],  "1110110111");
+	strcpy(maze[7],  "1100111001");
+	strcpy(maze[8],  "1100000011");
+	strcpy(maze[9],  "1111001111");
+	strcpy(maze[10], "1111001111");
+
+}
+
+void Render()
+{
 
 }
 
 
 int main()
 {
-	char key = 0;
-	int x = 2;
-	int y = 5;
 
-	GotoXY(x, y);
-	printf("☆");
-
-	while (1)
-	{
-		system("cls");
-		GotoXY(x, y);
-		printf("☆");
-		
-		if (_kbhit()) // 키보드 입력 확인 (true/false)
-		{
-			key = _getch(); // key 입력을 받아주는 함수
-
-			if (key == -32)
-			{
-				key = _getch();
-			}
-
-			switch (key)
-			{
-			case up: if (y > 0) { y--; }
-				break;
-			case LEFT :
-				if (x > 0)
-				{
-					x -= 2;
-				}
-				break;
-			case RIGHT: x += 2;
-				break;
-			case DOWN: y++;
-				break;
-			//default: printf("key 변수의 값 : %d\n", key);
-			//break;
-			}
-
-		}      
-	}
-
-	return 0;
+ 	return 0;
 }
