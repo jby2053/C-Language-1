0e2034965ea4743083a10c28e5f1556fc53ece62
jby2053 <jby2051@naver.com>
2024-01-17 21:46:38 +0900

    (2024.1.17) 미로 찾기 게임

--- Program/Solution.c
+++ Program/Solution.c
@@ -1,24 +1,52 @@
-#define _CRT_SECUER_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <conio.h>
 #include <string.h>
+#include <windows.h>
+
+#define up 72
+#define LEFT 75
+#define RIGHT 77
+#define DOWN 80
 
 #define WIDTH 11
 #define HEIGHT 11
 
 char maze[WIDTH][HEIGHT];
 
+
+
+
+void GotoXY(int x, int y)
+{
+	// x, y 좌표 설정
+	COORD position = { x,y };
+
+	// 커서 이동 함수
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+
+}
+
+typedef struct Player
+{
+	int x;
+	int y;
+	const char* shape;
+
+}Player;
+
 void CreatMaze()
 {
-	// 0 : 빈 공간 (" ")
-	// 1 : 벽 (▒)
+	// 0 : 빈 공간 ("  ") <--두 칸 띄우기
+	// 1 : 벽 (▤)
 	// 2 : 탈출구 (♥)
-	strcpy(maze[0],  "1111100011");
-	strcpy(maze[1],  "1111000111");
-	strcpy(maze[2],  "1100010011");
-	strcpy(maze[3],  "1001111001");
-	strcpy(maze[4],  "0011111100");
+	strcpy(maze[0],  "1111111111");
+	strcpy(maze[1],  "1100001100");
+	strcpy(maze[2],  "1100000011");
+	strcpy(maze[3],  "1001110001");
+	strcpy(maze[4],  "0001111001");
 	strcpy(maze[5],  "1100110011");
-	strcpy(maze[6],  "1110110111");
+	strcpy(maze[6],  "1110110011");
 	strcpy(maze[7],  "1100111001");
 	strcpy(maze[8],  "1100000011");
 	strcpy(maze[9],  "1111001111");
@@ -26,14 +54,91 @@ void CreatMaze()
 
 }
 
+void keyboard(char map[WIDTH][HEIGHT],Player * player)
+{
+	char key = 0;
+	
+		if (_kbhit()) // 키보드 입력 확인 (true/false)
+		{
+			key = _getch(); // key 입력을 받아주는 함수
+
+			if (key == -32)
+			{
+				key = _getch();
+			}
+
+			switch (key)
+			{
+			case up: if (y > 0) { y--; }
+				break; 
+
+			case LEFT:if (x > 0) { x -= 2;}
+				break;
+
+			case RIGHT: x += 2;
+				break;
+
+			case DOWN: y++;
+				break;
+
+				//default: printf("key 변수의 값 : %d\n", key);
+				//break;
+			}
+		}
+}
+
+
+
+
+
 void Render()
 {
+	for (int i = 0; i < WIDTH; i++)
+	{
+		for (int j = 0; j < HEIGHT; j++)
+		{
+			if (maze[i][j] == '0')
+			{
+				printf("  ");
+			}
+			else if (maze[i][j] == '1')
+			{
+				printf("▤");
+			}
+			else if (maze[i][j] == '2')
+			{
+				printf("♥");
+			}
+					
+		}
 
+		printf("\n");
+
+	}
 }
 
 
 int main()
 {
+	Player player  = { 3, 3, "＠" };
 
+	// 1. 맵 데이터를 생성합니다.
+	CreatMaze();
+
+	while (1)
+	{
+
+		// 2. 맵 데이터에 있는 정보를 토대로 출력합니다.
+		Render();
+
+		GotoXY(player.x, player.y);
+		printf("%s", player.shape);
+
+
+		Sleep(5000);
+		system("cls");
+		
+	}
+	 
  	return 0;
 }
